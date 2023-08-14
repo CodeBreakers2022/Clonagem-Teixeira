5f8a36093806c9f8e8dded20a69329a392904bd9
brunagtmaia <brunagmaia05@gmail.com>
2023-08-14 14:40:24 -0300

    correção navbar e footer

--- scripts/global.js
+++ scripts/global.js
@@ -1,4 +1,5 @@
 const menu_icon = document.getElementById('menu-icon');
+
 const menu_container = document.getElementById('navbar-small-container');
 const footer = document.getElementById('footer');
 const lines = document.querySelectorAll('.line'); // Usando querySelectorAll para obter todos os elementos
@@ -17,5 +18,25 @@ menu_icon.addEventListener('click', function () {
         lines.forEach(line => {
             line.style.backgroundColor = '#333';
         });
+    }
+});
+const ajudaHorarios = document.getElementById("ajuda-horarios");
+const menuAjuda = document.getElementById('menu-suspenso-ajuda');
+ajudaHorarios.addEventListener('click', function (){
+    ajudaHorarios.classList.toggle('open');
+    if(this.classList.contains('open')){
+        menuAjuda.style='display: flex';
+    }else{
+        menuAjuda.style='display: none';
+    }
+});
+const login = document.getElementById("login");
+const menuLogin = document.getElementById('menu-suspenso-login');
+login.addEventListener('click', function (){
+    login.classList.toggle('open');
+    if(this.classList.contains('open')){
+        menuLogin.style='display: flex';
+    }else{
+        menuLogin.style='display: none';
     }
 });
